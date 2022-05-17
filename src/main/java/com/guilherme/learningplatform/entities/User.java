fb700f2b01b9b9045a0cd0a811d097eaa4e2faad
Guilherme Costa Sales <guicosal@gmail.com>
2022-05-17 11:22:04 -0300

    Entity Notification and seed

--- src/main/java/com/guilherme/learningplatform/entities/User.java
+++ src/main/java/com/guilherme/learningplatform/entities/User.java
@@ -3,9 +3,7 @@ package com.guilherme.learningplatform.entities;
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -28,6 +26,9 @@ public class User implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Notification> notifications = new ArrayList<>();
 
     public User() {
     }
